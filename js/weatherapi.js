c3ad07d8fbf540cbf0fc6a80de497355ece58ef7
sonorangirl <manahan.erin@gmail.com>
2016-07-02 00:36:52 -0700

    Fixed icon prefix again

--- js/weatherapi.js
+++ js/weatherapi.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 		$.getJSON('js/weather-icons.json', function(icons) {
 			weatherIcons = icons;
 
-			var prefix = 'wi wi-owm-';
+			var prefix = 'wi wi-';
 			var code = data.weather[0].id;
 			var icon = weatherIcons[code].icon;
 
