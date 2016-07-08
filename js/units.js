855ca9695d2005b7251e324d772a6206acda9044
sonorangirl <manahan.erin@gmail.com>
2016-07-07 17:01:06 -0700

    Refined units.js

--- js/units.js
+++ js/units.js
@@ -2,33 +2,38 @@
 $(document).ready(function() {
 
 //Allow user to change units
-	function toCelsius() {
+	var unitStatus = "F";
+	var newTemperature;
+
+	function changeUnits() {
 		var oldTemperature = $('.current-temp').html();
 		oldTemperature = oldTemperature.substring(0, oldTemperature.length - 1);
-		console.log(oldTemperature);
-		var newTemperature = (Number(oldTemperature)  -  32)  *  (5 / 9);
-		console.log(newTemperature);
+		if (unitStatus === "F") {
+			newTemperature = (Number(oldTemperature)  -  32)  *  (5 / 9);
+			unitStatus = "C";
+		} else if (unitStatus === "C") {
+			newTemperature = Number(oldTemperature)  *  (9 / 5) + 32;
+			unitStatus = "F";
+		}
 		newTemperature = Math.round(newTemperature).toString() + '&deg;';
 		$('.current-temp').html(newTemperature);
-		console.log(newTemperature);
-	}
-
-	function toFahrenheit() {
-		var oldTemperature = $('.current-temp').html();
-		oldTemperature = oldTemperature.substring(0, oldTemperature.length - 1);
-		var newTemperature = Number(oldTemperature)  *  (9 / 5) + 32;
-		newTemperature = Math.round(newTemperature).toString() + '&deg;';
-		$('.current-temp').html(newTemperature);
-		console.log(newTemperature);
-
 	}
 
 	//Change units when user clicks options
 	$('#temp-units label[id="Celsius"]').click(function() {
-		toCelsius();
+		if (unitStatus === "F") {
+			changeUnits();
+		}
 	});
+
 	$('#temp-units label[id="Fahrenheit"]').click(function() {
-		toFahrenheit();
+		if (unitStatus === "C") {
+			changeUnits();
+		}
 	});
 
 });
+
+
+
+
